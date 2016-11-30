907d4e4eab53a6f6bdfd443ce99ac440f1d2e429
Nick Mertens <nick.mertens@fphcare.co.nz>
2016-11-30 17:40:02 +1300

    Add beginnings of create account page

--- ScrumMastery/views.py
+++ ScrumMastery/views.py
@@ -1,15 +1,43 @@
-from flask import render_template, url_for
+from datetime import datetime
+from flask import render_template, url_for, request, redirect
 
 from ScrumMastery import app
 
+accounts = []
+
+
+def create_new_account(email, password):
+    accounts.append(dict(
+        email = email,
+        password = password,
+        created = datetime.utcnow()
+    ))
+    return
+
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html', title="Home",
+    return render_template('index.html',
+                           title="Home",
                            heading="Scrum Mastery",
                            introduction=["Scrum Masters + Dev Team = Happiness!",
                                 "This is an app designed to help you keep track of all the things you want to keep \
                                 track of as a Scrum Master, and stay in touch with your team!"])
+
+
+@app.route('/create-account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        create_new_account(email, password)
+        return redirect(url_for('index'))
+
+    return render_template('create_account.html',
+                           title="Create Account",
+                           heading="Create an Account",
+                           introduction=["Create an account and be like brah, yo.", accounts])
 
 
 @app.errorhandler(404)
